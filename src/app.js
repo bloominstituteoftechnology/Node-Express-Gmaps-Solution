cf81696c7100d7014c1252f9034f0f3e46b08cf5
ivanmora <iam.ivan.mora@gmail.com>
2018-03-08 10:49:40 -0600

    Make helper functions

--- src/app.js
+++ src/app.js
@@ -12,26 +12,12 @@ const URI_TEXT_SEARCH = 'https://maps.googleapis.com/maps/api/place/textsearch/j
 const URI_PLACE_DETAILS = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=';
 
 app.get('/place', (req, res) => {
-  let search = req.query.search.split(' ');
-  search = search.join('+');
-  const searchUrl = URI_TEXT_SEARCH + search + '&key=' + KEY_GMAPS_PLACES;
-  console.log("url", searchUrl);
-  fetch(searchUrl, { method: 'GET'})
-    .then(places => places.json())
-    .then(places => {
-      const placeId = places.results[0].place_id;
-      const detailsUrl = URI_PLACE_DETAILS + placeId + '&key=' + KEY_GMAPS_PLACES;
-      fetch(detailsUrl)
-        .then(details => details.json())
-        .then(details => {
-          res.status(STATUS_SUCCESS);
-          res.send(details.result);
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(STATUS_USER_ERROR);
-          res.send({ err: err} );
-        });
+  getIds(req.query.search)
+    .then(ids => [ids[0]])
+    .then(getDetails)
+    .then(details => {
+      res.status(STATUS_SUCCESS);
+      res.send( {places: details} );
     })
     .catch(err => {
       console.log(err);
@@ -41,37 +27,51 @@ app.get('/place', (req, res) => {
 });
 
 app.get('/places', (req, res) => {
-  const search = req.query.search;
-  const searchUrl = URI_TEXT_SEARCH + search + '&key=' + KEY_GMAPS_PLACES;
-
-  fetch(searchUrl)
-    .then(places => places.json())
-    .then(places => {
-      placeIds = places.results.map(place => place.place_id);
-      details = placeIds.map(id => {
-        const detailsUrl = URI_PLACE_DETAILS + id + '&key=' + KEY_GMAPS_PLACES;
-        return fetch(detailsUrl)
-          .then(detailed => detailed.json())
-          .then(detailed => detailed.result);
-      });
-
-      Promise.all(details)
-        .then(details => {
-          res.status(STATUS_SUCCESS);
-          res.send( {places: details} )
-        })
-        .catch(err => {
-          console.log(err)
-          res.status(STATUS_USER_ERROR);
-          res.send( {err: err});
-        });
+  getIds(req.query.search)
+    .then(getDetails)
+    .then(details => {
+      res.status(STATUS_SUCCESS);
+      res.send( {places: details} );
     })
     .catch(err => {
-      console.log(err)
       res.status(STATUS_USER_ERROR);
       res.send( {err: err} );
     });
 });
+
+function getIds(query) {
+  return new Promise((resolve, reject) => {
+    const searchUrl = URI_TEXT_SEARCH + query + '&key=' + KEY_GMAPS_PLACES;
+    fetch(searchUrl)
+      .then(places => places.json())
+      .then(places => {
+        const ids = places.results.map(place => place.place_id);
+        resolve(ids);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+
+function getDetails(ids) {
+  return new Promise((resolve, reject) => {
+    const details = ids.map(id => {
+    const detailsUrl = URI_PLACE_DETAILS + id + '&key=' + KEY_GMAPS_PLACES;
+    return fetch(detailsUrl)
+      .then(detailed => detailed.json())
+      .then(detailed => detailed.result);
+    });
+
+    Promise.all(details)
+      .then(details => {
+        resolve(details);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
 
 app.listen(PORT, err => {
   if (err) {
