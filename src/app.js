d9659ea2a7a16dd22afedbb86d57afbe0830c304
ivanmora <iam.ivan.mora@gmail.com>
2018-03-08 09:58:27 -0600

    Update key config structure to handle more keys

--- src/app.js
+++ src/app.js
@@ -7,20 +7,20 @@ const app = express();
 const PORT = config.port;
 const STATUS_SUCCESS = 200;
 const STATUS_USER_ERROR = 422;
-const KEY_GMAPS = config.gmaps.apiKey
+const KEY_GMAPS_PLACES = config.gmaps.apiKeys.places;
 const URI_TEXT_SEARCH = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=';
 const URI_PLACE_DETAILS = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=';
 
 app.get('/place', (req, res) => {
   let search = req.query.search.split(' ');
   search = search.join('+');
-  const searchUrl = URI_TEXT_SEARCH + search + '&key=' + KEY_GMAPS;
+  const searchUrl = URI_TEXT_SEARCH + search + '&key=' + KEY_GMAPS_PLACES;
   console.log("url", searchUrl);
   fetch(searchUrl, { method: 'GET'})
     .then(places => places.json())
     .then(places => {
       const placeId = places.results[0].place_id;
-      const detailsUrl = URI_PLACE_DETAILS + placeId + '&key=' + KEY_GMAPS;
+      const detailsUrl = URI_PLACE_DETAILS + placeId + '&key=' + KEY_GMAPS_PLACES;
       fetch(detailsUrl)
         .then(details => details.json())
         .then(details => {
@@ -42,14 +42,14 @@ app.get('/place', (req, res) => {
 
 app.get('/places', (req, res) => {
   const search = req.query.search;
-  const searchUrl = URI_TEXT_SEARCH + search + '&key=' + KEY_GMAPS;
+  const searchUrl = URI_TEXT_SEARCH + search + '&key=' + KEY_GMAPS_PLACES;
 
   fetch(searchUrl)
     .then(places => places.json())
     .then(places => {
       placeIds = places.results.map(place => place.place_id);
       details = placeIds.map(id => {
-        const detailsUrl = URI_PLACE_DETAILS + id + '&key=' + KEY_GMAPS;
+        const detailsUrl = URI_PLACE_DETAILS + id + '&key=' + KEY_GMAPS_PLACES;
         return fetch(detailsUrl)
           .then(detailed => detailed.json())
           .then(detailed => detailed.result);
